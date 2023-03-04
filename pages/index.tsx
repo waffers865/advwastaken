2d0da852c58ae58f97e2901bc02f8df88ae40dd0
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-04 14:35:05 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen bg-[#111] flex-col items-center justify-center py-2">
       <Head>
         <title>ADV</title>
         <link rel="shortcut icon" href="/logo.png" />
