d3732e923a81069b2a08778964eab8d4d1a332ff
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-04 07:11:35 +0530

    Update xye26jigsgvxd.tsx

--- pages/xye26jigsgvxd.tsx
+++ pages/xye26jigsgvxd.tsx
@@ -9,29 +9,8 @@ const Home: NextPage = () => {
         <title></title>
       </Head>
       <p className="text-[1.5rem] text-white opacity-80 pop">
-      ummm... <br/><br/>
-      Sorry <br/><br/>
-      main apne parents ko yeh bolte hue sunna hu ki voh mereko yeh umr se bacchana chahte the jab baccha log galli aur vahi sab ke baare mein janna aur sikhna shuru karte hai, mere parents mereko C.S. mein itna interest karwa diye ki yeh sab ke liye time hi nahi raha aur vaise bhi main apna pichla school mein lonely rahna prefer karta tha (I mean hu, abhi bhi prefer vahi jyada karta hu). <br/><br/>
-      Mummy se pata chala ki ek boy aur girl ka mindset mein zameen aasman ka fark hota hai <br/><br/>
-      mereko khud pe accha nahi lag rha ki main itna din aap mere jaise nahi ho yeh baat ignore karke, aapke saath ekdam boy jaisa treat karrha tha <br/><br/>
-      bhale hi apka aur mera mindset, likes, dislikes same hosakta hai lekin gender thoda alag mindset kardeta hai <br/><br/>
-      I've always liked your soul and not your body. <br/><br/>
-      aap acche insaan ho, kisi aur ke liye apna life matt waste kijiega (bhale hi voh mere jaisa koi janwar bhi kyu naa hoo)<br/><br/>
-      Apko toh pata hi hoga main yeh sab baat pe jyada dhayan nahi deta, "I wish you were a boy", yahi ek line sab kuch represent kardeta hai 😅<br/><br/>
-      Sorry, and I do respect your feelings<br/><br/>
-      I, as your brother, will always hold your hand, although not physically, but indirectly/virtually <br/><br/>
-      Wish karta hu apko mere se hazaro, croro, quadrillioo gunna accha insaan mile (iss baar gussa mein nahi accha se bol rha hu) <br/><br/>
-      OneDrive mein photos upload karr raha tha, abhi pata chala ki 19 kya hai, Happy belated birthday sista! <br/><br/>
-      mera life ka phela badaaaaaaaaaaaaaa sponsorship mila ($75, samjh nahi aarha kya kya kharidu, bought discord nitro & spotify premium) <br/><br/>
-      u made efforts for me? <br/><br/>
-      im doing the same <br/><br/>
-      imma bath, exrcise daily now. (texts like of my discord variant)<br/><br/><br/>
-      Still just 3 requests? <br/><br/>
-      Don't hurt my feelings too, I guranttee, after my mom's sayings, I'm never doing to your's <br/><br/>
-      Treat me as your brother, nothing else <br/><br/>
-      Study, get into AIIMS, I'll literally be the first one to celebrate your achievements by my heart <br/><br/>
-      If you've seen this, make your account public, post some stories or anything as a reply.
-      </p>
+      You forgot to make your account public?
+</p>
     </div>
   )
 }
