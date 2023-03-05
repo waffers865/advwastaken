81b0ae5c6a7ac2ebbe3ff9570eb6760b20999d32
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-06 02:14:49 +0530

    Update xye26jigsgvxd.tsx

--- pages/xye26jigsgvxd.tsx
+++ pages/xye26jigsgvxd.tsx
@@ -6,12 +6,8 @@ const Home: NextPage = () => {
   return (
     <div className="min-h-screen w-full bg-[#111] py-24 px-12">
       <p className="text-sm text-[#eee] pop">
-        Presenting you, <a className="text-blue-700 underline" href="https://waffers865.github.io/dav-ss/">https://waffers865.github.io/dav-ss/ </a>
-      , as your birthday gift from your brother. I've focused on speed rather than design for now. Currently, it's version 0.3, I guranttee till v1.0, it'd get stable from UI to security, everything.
-Don't tell anyone I made this. Instead tell I (you) made this, to compete with ADV or something. <br/>
- And yeah, make your account public, talk to me with indications, I'm feeling like I'm getting ignored, if I take it seriously, then you already know what all things I can do. <br/>
-However, there are a little things that if I ever get to know then I'd have to break our friendship instantly without even a double thought, if I ever get to know you're telling anyone else about my personal or virtual life, then no matter what you're to me, I'll have to break our friendship.<br/>
-I can't take your name until you take mine, now don't make it obvious and take it infront of ppls, just an indication you took it thru some posts or anything. 
+        I made you a WebApp and still you chose to ignore me? You never actually had any feelings for me. <br/>
+I'VE DELETED IT. THANKS FOR EVERYTHING YOU'VE DONE FOR ME, BUT I'M REALLY FED UP NOW. LET ME TREAT YOU YHE WAY I TREAT OTHERS.
 </p>
     </div>
   )
