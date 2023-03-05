78d6197a2f20b709e884ae27aba132bc4db9a12e
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-05 10:50:52 +0530

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -14,7 +14,7 @@ const router = useRouter()
             <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
             <p className="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn't exist.Here are some helpful links:</p>
 
-            <div className="flex items-center mt-6 gap-x-3">
+            <div className="flex items-center justify-center mt-6 gap-x-3">
                 <button onClick={() => router.back()} className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
@@ -24,9 +24,7 @@ const router = useRouter()
                     <span >Go back</span>
                 </button>
 <Link href="/">
-                <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
-                    Take me home
-                </button>
+               
 </Link>
             </div>
         </div>
