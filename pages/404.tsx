7f6b77cc05126e873c037d4853b2fe66d667946b
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-05 10:59:20 +0530

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -16,7 +16,8 @@ const router = useRouter()
         <div className="wf-ull lg:w-1/2">
             <p className="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
             <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
-            
+            <p class="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn't exist.</p>
+
             <div className="flex items-center justify-center mt-6 gap-x-3">
                 <button onClick={() => router.back()} className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
