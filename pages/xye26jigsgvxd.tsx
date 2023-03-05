44a72007d7a1d17f2fcc5ee904ccb07a7ea2937c
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-05 21:43:15 +0530

    Update xye26jigsgvxd.tsx

--- pages/xye26jigsgvxd.tsx
+++ pages/xye26jigsgvxd.tsx
@@ -10,7 +10,8 @@ const Home: NextPage = () => {
       , as your birthday gift from your brother. I've focused on speed rather than design for now. Currently, it's version 0.3, I guranttee till v1.0, it'd get stable from UI to security, everything.
 Don't tell anyone I made this. Instead tell I (you) made this, to compete with ADV or something. <br/>
  And yeah, make your account public, talk to me with indications, I'm feeling like I'm getting ignored, if I take it seriously, then you already know what all things I can do. <br/>
-However, there are a little things that if I ever get to know then I'd have to break our friendship instantly without even a double thought, if I ever get to know you're telling anyone else about my personal or virtual life, then no matter what you're to me, I'll have to break our friendship. 
+However, there are a little things that if I ever get to know then I'd have to break our friendship instantly without even a double thought, if I ever get to know you're telling anyone else about my personal or virtual life, then no matter what you're to me, I'll have to break our friendship.<br/>
+I can't take your name until you take mine, now don't make it obvious and take it infront of ppls, just an indication you took it thru some posts or anything. 
 </p>
     </div>
   )
