0293d9cc637ef38ca55c30e27915edc30caef065
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-06 15:08:20 +0530

    Update xye26jigsgvxd.tsx

--- pages/xye26jigsgvxd.tsx
+++ pages/xye26jigsgvxd.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <div className="min-h-screen w-full bg-[#111] py-24 px-12">
       <p className="text-sm text-[#eee] pop">
       You're the first person who made me cry, and feel ignored. Congratulations 🎉, there are somethings you're gonna get in return for that too. Breaking all bonds with you no matter what you're to me. OK, joking again? It's funny 😂. Let's see if it's a joke and YOU HOPE IT WAS JUST A JOKE. Your WebApp was on update v0.7 and I really just don't care about it anymore. <br/>
-I realised you're coming between me and my academical life. There are only some stuffs in between if anyone comes, no matter whoever they're, I'm breaking all bonds whatsoever. You came in between me and my C.S life. I just don't care about you anymore. Yes it's me, I wear muffer all around my head, no virtual person knows about my irl activities except ppl like you. Anyways got my sponsorship money, you may find me in/around Katras-Bazar nowadays. Saionara.  
+I realised you're coming between me and my academical life. There are only some stuffs in between if anyone comes, no matter whoever they're, I'm breaking all bonds whatsoever. You came in between me and my C.S life. I just don't care about you anymore. Yes it's me, I wear muffer all around my head, no virtual person knows about my irl activities except ppl like you. Anyways got my sponsorship money, you may find me in/around Katras-Bazar nowadays. Enjoy with your new friends and let me enjoy with my new team Squad909, JUST LEAVE ME ALONE. Saionara.  
 </p>
     </div>
   )
