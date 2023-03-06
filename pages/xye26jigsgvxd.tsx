8251d7dd860f2ea59b3efc804a0c11f606955e80
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-06 18:33:39 +0530

    Update xye26jigsgvxd.tsx

--- pages/xye26jigsgvxd.tsx
+++ pages/xye26jigsgvxd.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
       Bruh, I can't really get you out of my mind, sis. You're literally one of my life's most trustable and favourite person. I won't hurt your feelings however how much you do to mine. <br/>
 I'm a boy I'm used to getting my feelings hurt, but you're different, I was once again treating you as a boy, you need to understand I've no experience talking to, ughhhhh, <you know this word>, You hate me? I don't really care, I'm gonna treat you the same way I've ever done. Sorry, for my all misdeed. Thank you for everything you've done for me. <br/>
 Fun fact: I knew you won't login into my account ever, I've never changed it's password. I mean you're my sis, really. <br/>
-Bruh, that day, I turned around my head, saw you literally in my hairstyle, smiling, one of the best moments of my life, my sister.
+Bruh, that day, I turned around my head, saw you literally in my hairstyle (my dream person~ people of my mindset, to whom I could talk till eternity), smiling, one of the best moments of my life, my sister.
 Look, if you promise me to never talk about marriage and all, I'll literally talk, play with you IRL, but I think I may be different IRL, won't talk this much. 
 </p>
     </div>
