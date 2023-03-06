a3405c1aa7f9a2458258e22acd7ad2ef74fdbb5b
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-06 09:48:57 +0530

    Update xye26jigsgvxd.tsx

--- pages/xye26jigsgvxd.tsx
+++ pages/xye26jigsgvxd.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className="min-h-screen w-full bg-[#111] py-24 px-12">
       <p className="text-sm text-[#eee] pop">
-      Did I really just saw you in my dream ☠️, Sorry for being rude over there too, you probably haven't seen but I had a fight with Asim, so my mood wasn't alright at that time. Again, Sorry. Bruh, dream? maybe only I'd seen.<br/>
+      Did I really just saw you in my dream ☠️, Sorry for being rude over there too, you probably haven't seen but I had a fight with Asim, so my mood wasn't alright at that time. And your kinda offensive line just triggered my anger. Again, Sorry. Bruh, dream? maybe only I'd seen.<br/>
 Ignore this, if you didn't see my dream ☠️☠️  
 </p>
     </div>
