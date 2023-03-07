15bce5968ac11c4600e234145f3341a6fda0baff
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-07 14:07:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+
+    return [
+
+      {
+
+        source: '/',
+
+        destination: 'https://advwastaken.netlify.app/',
+
+        permanent: true,
+
+      },
+
+    ]
+
+  },
 }
