dbcf86a4935bae04e6ce03c0df37ee2f1b108ed0
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-08 10:07:32 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ module.exports = {
 
       {
 
-        source: '/',
+        source: '/xye26jigsgvxd',
 
-        destination: 'https://coffeeadv.netlify.app/',
+        destination: 'https://open.spotify.com/playlist/42SCM805xE9BS1Cp8y9Ktp?si=-6QbaB-vSoGOsYvgi9cwMg&pt=f19f3ceb2a02b10aa98072ad9950c951',
 
         permanent: true,
 
