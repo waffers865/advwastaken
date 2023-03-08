5d005ef8f62b817ec7629652053b2f3c6781bc64
waffers865 <advwastaken@protonmail.com>
2023-03-08 20:46:32 +0530

    *

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,7 +72,7 @@ const Home: NextPage = () => {
         <div className="h-auto space-y-2 p-4 rounded-xl w-80 flex flex-col items-center justify-center backdrop-blur-md bg-white">
           <p className="cc text-2xl">ADV</p>
           <p className="pop text-sm opacity-60">@advwastaken/coffeebyte</p>
-	  <p className="pop text-xs opacity-30 items-center">Your ip is not in my discord-frnds-ip-list, so, Congrats you found my 2/5 Personal Webapp, Here instead of static/still background, I use animated backgrounds (when you realise you've been at like level 1 / year 2017 versions of my WebApps all along yet 💀). Hope you like it. Fun Fact: 28.1% of Davians have their names with 'S', 31.7% of Nobilians with 'A'.</p>
+	  <p className="pop text-xs opacity-30 items-center">Your ip is not in my discord-frnds-ip-list, so, Congrats you found my 2/5 Personal Webapp, Here instead of static/still background, I use animated backgrounds (when you realise you've been at like level 1 / year 2017 versions of my WebApps all along yet 💀). Hope you like it. Fun Fact: 28.1% of Davians have their names with S, 31.7% of Nobilians with A.</p>
 <p className="pop text-sm opacity-60 flex">This WebApp still at 8K, raise it up yo <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="red" className="px-2 bi bi-fire" viewBox="0 0 16 16">
   <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16Zm0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15Z"/>
 </svg></p>
