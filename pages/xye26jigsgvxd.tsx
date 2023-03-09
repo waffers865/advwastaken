b3d18eb97f612341510fe9459ff0c9ea51129e33
waffers865 <advwastaken@protonmail.com>
2023-03-09 21:24:16 +0530

    *

---
+++ pages/xye26jigsgvxd.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <div className="min-h-screen w-full py-24 px-12 bg-[#111] text-white text-sm pop">
+      <p>You did selected me from millions, but the same also goes here. If I select something then it's selected forever, no way someone's ever getting in between us, just don't believe any third person ever.</p>
+      <p>I'm going back to my world, class 12th basically all about electricity (my love). Just as the way we were before I got to know you're here. So, if I get my mind into something I usually try to complete with all of my mind(you must've known till now, I hate theories (C.S, Physics, Chemistry), love practicals.</p>
+      <p>So, just 1 year from now on (actually lesser), don't be on insta anymore, I know thousands of distractions are on it's way, but you know, although I was the one to distract farthest, I actually didn't do it intentionally. Hope you forgive me. About me? You're my favourite as always. I promise meeting you after we into our dream lives. If you ever had any true feelings for me, you're going to open a "free-hospital" for poor peoples.</p>
+      <p>I can't really get to know what's on your mind, mate. There's no way I can bypass private insta acc, you may have to make your account public to tell your thoughts. Last day, tell all of your thoughts. If you ever feel I'm fading away, it's just an illusion or just your dream. Falling, Sunflower, Mood~ Spotify songs that define my feelings for ya. We are from different cultures, religions but we're humans anyways.</p>
+    </div>
+    
+  )
+}
+
+export default Home;
