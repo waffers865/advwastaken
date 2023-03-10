0a37ee4c2270a540468490fe625d7cc6bb740aea
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-11 03:53:34 +0530

    Update msg.ts

--- pages/api/msg.ts
+++ pages/api/msg.ts
@@ -14,6 +14,6 @@ export default function handler(
 
 ) {
 
-  res.status(200).json({ msg: 'you\'re indirectly breaking my heart by just recovering your insta account, it indicates you don\'t value my words, not meaning to say cut-off from your daily life as well but as you already know NEET needs sacrifice, even if the sacrifice is me, it\'s worth it, for breaks, level up your talent/watch some YouTube, and yeah, i guess someone else is also watching that secret route, unusual traffic in that route, did you really gave that route to someone? or it\'s just Google/Yandex bots? if you gave it someone and they get to know about dark cof im done' })
+  res.status(200).json({ msg: 'you\'re indirectly breaking my heart by just recovering your insta account, it indicates you don\'t value my words, not meaning to say cut-off from your daily life as well but as you already know NEET needs sacrifice, even if the sacrifice is me, it\'s worth it, for breaks, level up your talent/watch some YouTube, and yeah, i guess someone else is also watching that secret route, unusual traffic in that route, did you really gave that route to someone? or it\'s just Google/Yandex bots? if you gave it someone and they get to know about dark cof im done, it was RSA encrypted with decryption on just your ip, but still if there\'s any technical guilt and someone else comes to know, ill be in trouble although im the head'})
 
 }
