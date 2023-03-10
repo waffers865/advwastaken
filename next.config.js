a8e7cebee736af9bc007ce2a43bc5cb592f201dc
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-10 18:58:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,15 @@ module.exports = {
 
     return [
 
+{
 
+        source: '/xye26jigsgvxd',
+
+        destination: 'https://youtube.com/playlist?list=PLcp1KqeP2WdmbZhAocHQMa3xcxS35sZ0W',
+
+        permanent: true,
+
+      },
             {
 
         source: '/',
