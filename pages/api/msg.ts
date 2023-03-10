0e9ae1da192294809ccd26613add7895baf7be02
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-11 04:36:38 +0530

    Update msg.ts

--- pages/api/msg.ts
+++ pages/api/msg.ts
@@ -14,6 +14,5 @@ export default function handler(
 
 ) {
 
-  res.status(200).json({ msg: 'you\'re indirectly breaking my heart by just recovering your insta account, it indicates you don\'t value my words, not meaning to say cut-off from your daily life as well but as you already know NEET needs sacrifice, even if the sacrifice is me, it\'s worth it, for breaks, level up your talent/watch some YouTube, and yeah, i guess someone else is also watching that secret route, unusual traffic in that route, did you really gave that route to someone? or it\'s just Google/Yandex bots? if you gave it someone and they get to know about dark cof im done, it was RSA encrypted with decryption on just your ip, but still if there\'s any technical guilt and someone else comes to know, ill be in trouble although im the head and you need to know that message is far more secret that even any passwords or something, i trust you, no one else irl, olease never tell about it anyone'})
-
+  res.status(200).json({ msg: 'oke recover ypur account, talk with your friends, i don\'t want to make any restrictions to your life, mate, i know it doesn\'t matter how much i ever care for you it\'s aleays going to be unmatched with your parent\'s care for you, not even close'})
 }
