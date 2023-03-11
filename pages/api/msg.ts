9957aeeba093741f4020b42a2258b6b40051ff3d
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-11 10:52:47 +0530

    Update msg.ts

--- pages/api/msg.ts
+++ pages/api/msg.ts
@@ -14,5 +14,5 @@ export default function handler(
 
 ) {
 
-  res.status(200).json({ msg: 'oke recover your account, talk with your friends, i don\'t want to make any restrictions to your life either, mate, i know it doesn\'t matter how much i ever care for you it\'s aleays going to be unmatched with your parent\'s care for you, not even close, still i got to know you do value me, thanks!'})
+  res.status(200).json({ msg: 'oke recover your account, talk with your friends, i don\'t want to make any restrictions to your life either, mate, i know it doesn\'t matter how much i ever care for you it\'s always going to be unmatched with your parent\'s care for you, not even close, still i got to know you do value me, thanks! i took the risk and ended up getting a soul that\'s actually true to me, same here, study mate, its hard but imagine the life you have always been dreaming of'})
 }
