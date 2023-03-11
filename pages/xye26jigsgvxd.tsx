749a19d67ed7a741daf33125ecdf1f8e09689e40
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-12 01:21:44 +0530

    Create

---
+++ pages/xye26jigsgvxd.tsx
@@ -0,0 +1,45 @@
+import type { NextPage } from "next";
+
+import Head from "next/head";
+
+import Link from "next/link";
+
+const Home: NextPage = () => {
+
+  return (
+
+    <div className="min-h-screen py-24 px-12 w-full bg-gradient-to-br from-violet-600 to-red-400 pop space-y-8 text-white/90 text-sm">
+
+      <p>Okay, I admit it SS, I've talked to you before on long texts just for increasing my typing speed, but now I feel like I'm actually typing from my mind and heart.</p>
+
+      <p>Ava by Famy on Spotify, I know any song could never deliver you my true feelings for you, but this song has the background song that kinda matches how I'm feeling right now. </p>
+
+      <p>I know I can't help you always, you have life without me and the same goes for me.</p>
+
+      <p>You know, I'm my family's little son, my elder brother and I, never be together even for once, we're always in fight, it's like we fight in every 10 mins, but I saw him crying behind the door, when he got to know I got low marks in class 8th. That day I had realised it's no matter how much we fight each other, at the end of the day we care for each other.</p>
+
+      <p>(I'M NOT TRYING TO MAKE ANY EMOTIONAL ATTACHMENT OR ANYTHING LIKE MAKING YOU CRY) I've always been in fight with you, but unlike others, to whom if I ever get in fight with never even look at them back again, I always turned back for you.</p>
+
+      <p>Have you ever realised how much awkward it is, that in this world of millions of peoples you only had to choose me? and for me, you? 😂</p>
+
+      <p>I can't take your name or either start talking to you in real because of an oath given to my mother.</p>
+
+      <p>I know you might've thought of me that I'd slip for you by your body (that day, you were standing infront of me, slanting on teacher's desk, looking outside, and I was talking to Akshat), but you didn't know at that time, how my parents raised me differently from the other world, although they knew one day I had to get matured.</p>
+
+      <p>Spent my $75, thought of buying something for you, but couldn't because it could be a huge distraction for you. You know why I didn't build your web? I know I gave an excuse of "some discord things", but in reality, I didn't want to distract you.</p>
+
+      <p>You perhaps know I know browser automation, I admit I had put some async api callbacks on your profiles, in simple, I get to know your profile's activities. But know, I'm removing it. It's just opening a cage, I didn't meant to stalk you or anything like that, but I cared for you since I gave you password and you actually turned out to be trustworthy. Yes, now I won't get notified, when you're recovering your account, or disabling it. On YouTube I saw, {"<your gender>*"} don't like {"<my gender>"} that talk to {"<again>*"}, and maybe that's why I left talking to Emy and Alice.</p>
+
+      <p>You know I've tears in my eyes right now, even if you don't believe, I just want to tell you that among these thousands of distractions (and I won't decline I'm also one of "these"), Please, focus on your studies snd start a new life in 12th. You know I heard some students talking sh*t about you infront of me, I thought of slapping them right there, but I couldn't do so and I'm sorry for that, I can't reveal their names either. You know I really hope when you get into AIIMS, you get someone far better than me, I realised mere jaisa neech aadmi don't deserves even to be around you. Because of your attractive body, you may get many people to follow you, but your soul really got a soul to get followed by. When I was about to join DAV, I had thought just another 2 years, I'll spend it alone with my discord friends but I had never thought, even in my dreams that god was sending a person like you in my life. Had you even in your dreams thought you're going to come across like a person like me? Interested in C.S, much more than anything else 😂 who's just going to change your personality forever (if you haven't realised you also speak in english nowadays, because my hindi is weak for real 💀) Maybe my longest and last ever message to you.</p>
+
+      <p>I'll never forget how much of a big risk I was about to take, risking all my WebApps, Apps, discord servers, and you just made me have faith on you more than anyone else.</p>
+
+      <p>*You have a much more beautiful life ahead* ~ ADV, 2023 - mark my words.</p>
+
+    </div>
+
+  )
+
+}
+
+export default Home;
