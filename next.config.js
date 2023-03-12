0f54f22b7ab53d9eb1b41180284acd5b1051017e
waffers865 <advwastaken@protonmail.com>
2023-03-12 21:11:56 +0530

    *

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/xye26jigsgvxd',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
 }
---
+++ pages/xye26jigsgvxd.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+    <p>sorry</p>
+    </div>
+  )
+}
+
+export default Home
