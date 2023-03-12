5ff892e673761778fbf12f194cb72e87b0f83e9e
waffers865 <advwastaken@protonmail.com>
2023-03-12 18:16:09 +0530

    *

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,13 @@ import { Fragment } from "react";
 const Home: NextPage = () => {
   return (
     <div>
-    <div className="h-4 w-full bg-[#CB4E38]"></div>
+    <div className="h-2 w-full bg-[#CB4E38]"></div>
     <div className="bg-[#18191B] min-h-screen w-full flex items-center justify-center">
       <Head>
         <title>FireZetta</title>
         <link rel="shortcut icon" href="/logo.png" />
       </Head>
-        <img className="h-12 w-auto" src="/fire.gif" alt="loader"/>
+        <img className="h-64 w-auto" src="/fire.gif" alt="loader"/>
     </div>
     </div>
   );
