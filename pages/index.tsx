67757d317544caae88a4db99edfe1f6852ff8b57
waffers865 <advwastaken@protonmail.com>
2023-03-12 20:50:39 +0530

    *

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,13 +8,13 @@ const Home: NextPage = () => {
   return (
     <div>
     <div className="h-2 w-full bg-[#CB4E38]"></div>
-    <div className="bg-[#18191B] min-h-screen w-full flex flex-col space-y-2 items-center justify-center">
+    <div className="bg-[#18191B] min-h-screen w-full flex flex-col space-y-2 items-center justify-center px-8">
       <Head>
         <title>FireZetta</title>
         <link rel="shortcut icon" href="/logo.png" />
       </Head>
         <img className="h-64 w-auto" src="/fire.gif" alt="loader"/>
-        <p className="text-md pop text-[#CB4E38]">Cut/Fired-off my relations with IRL (from secret routes to encrypted chats).</p>
+        <p className="text-sm pop text-[#CB4E38]">Cut/Fired-off my relations with IRL (from secret routes to encrypted chats).</p>
     </div>
     </div>
   );
