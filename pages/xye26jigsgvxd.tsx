da56f7939c405bfad36b1edb177b85b03adc3817
waffers865 <advwastaken@protonmail.com>
2023-03-12 21:20:50 +0530

    *

--- pages/xye26jigsgvxd.tsx
+++ pages/xye26jigsgvxd.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Home: NextPage = () => {
   return (
     <div>
-    <p>sorry, u ain't like me, thought I'd chase behind although u hurted me the most? just my irl v1 for u, find somebody better than me, cuz there's just no way im ever coming back</p>
+    <p>sorry, u ain't like me, thought I'd chase behind although u hurted me the most? just my irl v1 for u, find somebody better than me, cuz there's just no way im ever coming back. I cared for you, thought of your marks and you in return tried to hurt me, thanks 😀</p>
     </div>
   )
 }
