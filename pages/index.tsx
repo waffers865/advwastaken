0c5ff971379efa99d5f1df5da94d632ba71c737b
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-13 17:33:25 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ const Home: NextPage = () => {
         <link rel="shortcut icon" href="/logo.png" />
       </Head>
         <img className="h-64 w-auto" src="/fire.gif" alt="loader"/>
+        <p className="text-sm underline pop decoration-[#CB4E38] text-[#eee]">They believe in quantity, I believe in quality. Don't think I care about some people whom I gave special routes yo. They tried to hurt me. I choose to remove them from my entire life ;) <br/>They surely do deserve hella lot better than me. Saionara, may the flame above burn all the relations with them 🙏</p>
     </div>
     </div>
   );
