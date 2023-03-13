bbc5d66f01798e42277dc183874466ada4b9f2eb
waffers865 <126883574+waffers865@users.noreply.github.com>
2023-03-13 18:55:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+
+    return [
+
+      {
+
+        source: '/xye26jigsgvxd',
+
+        destination: '/',
+
+        permanent: true,
+
+      },
+
+    ]
+
+  },
 }
