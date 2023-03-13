22a7aad5790027a534faabf7dac3c1524fdd6c24
waffers865 <advwastaken@protonmail.com>
2023-03-13 13:07:35 +0530

    *

--- pages/xye26jigsgvxd.tsx
+++ pages/xye26jigsgvxd.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Home: NextPage = () => {
   return (
     <div>
-    <p>please i beg u, pass 11th, you changed back ur hairstyle, tried to show you hate me, but i don't really care about it, i just don't care what happened till now, u hurted me, i hurted you, just nothing, but please pass class 11th, among d world of these many fake friends, i just can't accept your downfall even if u want to see me fail, u know at d end of d day alongwith Arush & Gaurav, u r d only person i care about but i can't really help u in exams, u need to do that urself just like us</p>
+    <p>please i beg u, pass 11th, you changed back ur hairstyle, tried to show you hate me, but i don't really care about it, i just don't care what happened till now, u hurted me, i hurted you, just nothing, but please pass class 11th, among d world of these many fake friends, i just can't accept your downfall even if u want to see me fail, u know at d end of d day alongwith Arush & Gaurav, u r d only person i care about but i can't really help u in exams, u need to do that urself just like us, i know ive been your distraction even in d time of exams, and i hope i had kept my mouth and action shut at that time, but i cant bring back the time either, listen, only for u, im disabling my WebApp although ive got a sponsorship for it and im not going to speak even a word anymore till i see you pass</p>
     </div>
   )
 }
